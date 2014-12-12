76b20714c8d6959e61659bb445f8216e2286ca17
Christopher Potts <cgpotts@gmail.com>
2014-12-12 12:29:26 -0800

    General utilities

---
+++ utils.py
@@ -0,0 +1,29 @@
+#/usr/bin/env python
+
+import numpy as np
+
+def rownorm(mat):
+    """Row normalization of a matrix"""
+    return np.divide(mat.T, np.sum(mat, axis=1)).T
+    
+def colnorm(mat):
+    """Column normalization of a matrix"""    
+    return np.divide(mat, np.sum(mat, axis=0))
+
+def safelog(vals):           
+    with np.errstate(divide='ignore'):
+        return np.log(vals)
+
+def display_matrix(mat, rnames=None, cnames=None, title='', digits=4):
+    """Utility function for displaying strategies to standard output."""
+    mat = np.round(mat, digits)
+    rowlabelwidth = 2 + max([len(x) for x in rnames+cnames] + [digits+2])
+    cwidth = 2 + max([len(x) for x in cnames] + [digits+2])
+    # Divider bar of the appropriate width:
+    print "-" * ((cwidth * len(cnames)) + rowlabelwidth)
+    print title
+    # Matrix with even-width columns wide enough for the data:
+    print ''.rjust(rowlabelwidth) + "".join([str(s).rjust(cwidth) for s in cnames])        
+    for i in range(mat.shape[0]):  
+        print str(rnames[i]).rjust(rowlabelwidth) + "".join(str(x).rjust(cwidth) for x in mat[i, :])
+    
