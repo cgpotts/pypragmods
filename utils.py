cc19a1ce9a45d6adab16a8a087feb56be41097eb
Christopher Potts <cgpotts@gmail.com>
2014-12-12 14:33:00 -0800

    Improved formatting and addition of powerset

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #/usr/bin/env python
 
 import numpy as np
+from itertools import combinations
 
 def rownorm(mat):
     """Row normalization of a matrix"""
@@ -17,7 +18,7 @@ def safelog(vals):
 def display_matrix(mat, rnames=None, cnames=None, title='', digits=4):
     """Utility function for displaying strategies to standard output."""
     mat = np.round(mat, digits)
-    rowlabelwidth = 2 + max([len(x) for x in rnames+cnames] + [digits+2])
+    rowlabelwidth = 2 + max([len(x) for x in rnames] + [digits+2])
     cwidth = 2 + max([len(x) for x in cnames] + [digits+2])
     # Divider bar of the appropriate width:
     print "-" * ((cwidth * len(cnames)) + rowlabelwidth)
@@ -26,4 +27,11 @@ def display_matrix(mat, rnames=None, cnames=None, title='', digits=4):
     print ''.rjust(rowlabelwidth) + "".join([str(s).rjust(cwidth) for s in cnames])        
     for i in range(mat.shape[0]):  
         print str(rnames[i]).rjust(rowlabelwidth) + "".join(str(x).rjust(cwidth) for x in mat[i, :])
-    
+        
+def powerset(x, minsize=1, maxsize=None):
+    result = []
+    if maxsize == None: maxsize = len(x)
+    for i in range(minsize, maxsize+1):
+        for val in combinations(x, i):
+            result.append(list(val))
+    return result
