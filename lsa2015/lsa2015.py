2770ba07d679aad596e5a461936d31d85290f0d0
Christopher Potts <cgpotts@gmail.com>
2014-12-13 11:45:43 -0800

    Clearer correspondence with the poster

--- lsa2015/lsa2015.py
+++ lsa2015/lsa2015.py
@@ -49,18 +49,25 @@ def generic_example(alpha=1.0, beta=1.0, disjunction_cost=1.0):
         temperature=1.0,
         alpha=alpha,
         beta=beta)
-    # Show the lexica:
-    lexica.display()
-    # Run the base model on the individual lexica so we can show those lower steps:
-    for lex in lexmats:
-        print "======================================================================"
-        print mod.lex2str(lex)
-        mod.run_base_model(lex, n=2, display=True, digits=2)        
-    # Run the anxious experts model:
-    langs = mod.run_expertise_model(n=3, display=True, digits=2)
+    ## Uncomment these lines for a fuller picture:
+    ## Show the lexica:
+    # lexica.display()
+    ## Run the base model on the individual lexica so we can show those lower steps:
+    # for lex in lexmats:
+    #     print "======================================================================"
+    #     print mod.lex2str(lex)
+    #     mod.run_base_model(lex, n=2, display=True, digits=2)             
+    ## Run the anxious experts model - display=True for a fuller picture:
+    langs = mod.run_expertise_model(n=2, display=False, digits=2)
+    # Look at the specific table we care about:
+    msg_index = mod.messages.index('A v X')
+    final_listener = langs[-1]
+    mod.display_joint_listener(final_listener[msg_index], digits=2)
+    return langs
         
 def Hurfordian_Contexts():
     generic_example(alpha=2.0, beta=1.0, disjunction_cost=1.0)
+    
     
 def Definitional_Contexts():
     generic_example(alpha=5.0, beta=7.0, disjunction_cost=0.01)
